78ea9702a505935089f0b93242b41294b27dc02a
MyMac <mymac@10-18-23-55.dynapool.wireless.nyu.edu>
2021-10-21 08:43:15 -0400

    deleted unused variable

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const router = express.Router();
 
 const homePage = require("./routes/index.js");
 const aboutPage = require("./routes/about.js");
