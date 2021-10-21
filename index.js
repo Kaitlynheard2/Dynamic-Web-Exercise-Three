7753c93212c6867dfa79e6cb12fadb7463c8993b
MyMac <mymac@10-18-23-55.dynapool.wireless.nyu.edu>
2021-10-21 08:36:15 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const router = express.Router();
+
+const homePage = require("./routes/index.js");
+const aboutPage = require("./routes/about.js");
+
+app.use(express.static("public"));
+
+app.use("/about", aboutPage);
+app.use("/", homePage);
+
+app.listen(port, () => {
+  console.log(`App Listening on Port ${port}`);
+});
